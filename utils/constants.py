dd9014c0c37813eca58f3e177891066f6b828d63
tridn <tridnguyen97@gmail.com>
2023-04-16 08:17:39 +0700

    change project directory

--- utils/constants.py
+++ utils/constants.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 cur_abs_path = Path(os.path.abspath(__file__))
 
-BASE_DIR = cur_abs_path.parents[2]
+BASE_DIR = cur_abs_path.parents[1]
 URL_FILE_DIR = os.path.join(BASE_DIR, "urls.txt")
 PROXY_FILE_DIR = os.path.join(BASE_DIR, "proxy.txt")
-MAINVIEW_DIR = os.path.join(BASE_DIR, "app/ui/MainView.glade")
-SETTINGVIEW_DIR = os.path.join(BASE_DIR, "app/ui/SettingView.glade")
+MAINVIEW_DIR = os.path.join(BASE_DIR, "ui/MainView.glade")
+SETTINGVIEW_DIR = os.path.join(BASE_DIR, "ui/SettingView.glade")
 PROXY_TYPES = [
     "HTTP",
     "SOCKS4",
